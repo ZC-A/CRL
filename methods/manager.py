63c79a2d2ae14b93f3b145cd3461ca94581e9699
ZC-A <57583928+ZC-A@users.noreply.github.com>
2023-04-03 14:16:40 +0800

    Update manager.py

--- methods/manager.py
+++ methods/manager.py
@@ -165,6 +165,33 @@ class Manager(object):
                 else:
                     self.moment.update(ind, hidden.detach())
             print(f"{name} loss is {np.array(losses).mean()}")
+            
+            for current_relation in memorized_samples:
+                tokens = []
+                current_tokens = memorized_samples[current_relation]
+                for token in current_tokens:
+                  tokens.append(torch.tensor(token['tokens']))
+                tokens = torch.stack([x.to(args.device) for x in tokens], dim=0)
+                #tokens = [torch.tensor(x['tokens'] for x in current_tokens)]
+                #print(tokens)
+                #tokens = torch.stack([x.to(args.device) for x in tokens], dim = 0)
+                fe, rp = encoder.bert_forward(tokens)
+                #print(proto_dict[current_relation])
+                #print(fe)
+        
+                for f in fe:
+                  loss = 0.0
+                  loss += torch.log(torch.tensor(F.cosine_similarity(f.unsqueeze(0), proto_dict[current_relation].to(args.device).unsqueeze(0)).item() + 1e-5))
+                  for relation in memorized_samples:
+                    if relation != current_relation:
+                      loss += torch.log(torch.tensor(1 - F.cosine_similarity(f.unsqueeze(0), proto_dict[relation].to(args.device).unsqueeze(0)).item() + 1e-5))
+                  log_losses.append(loss)
+            log_loss = torch.mean(torch.tensor(log_losses))
+            optimizer.zero_grad()
+            log_loss.backward()
+            print("proto_learn loss is {np.array(log_losses).mean()}")
+            torch.nn.utils.clip_grad_norm_(encoder.parameters(), args.max_grad_norm)
+            optimizer.step()
         for epoch_i in range(epochs):
             train_data(mem_loader, "memory_train_{}".format(epoch_i), is_mem=True)
 
